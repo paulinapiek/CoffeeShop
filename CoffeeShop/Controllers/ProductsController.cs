722d403a3e819a6e8301ffc82d791c0557194e3c
paulinapiek <piekarska.paulina97@gmail.com>
2023-12-07 18:46:17 +0100

    Dodaj pliki projektów.

---
+++ CoffeeShop/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using CoffeeShop.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShop.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IProductRepository productRepository;
+        public ProductsController(IProductRepository productRepository)
+        {
+           this.productRepository = productRepository;
+        }
+        public ActionResult Shop() 
+        {
+            return View(productRepository.GetAllProducts());
+        }
+
+    }
+}
---
+++ CoffeeShop/Data/CoffeeShopDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeShop.Data
+{
+    public class CoffeeShopDbContext : DbContext
+    {
+        public CoffeeShopDbContext(DbContextOptions<CoffeeShopDbContext>options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
